1a7198218dad7ce37c369fe8705a51a1b8344b35
NosiphoX <2708798@students.wits.ac.za>
2026-01-27 21:47:16 +0200

    Skeleton

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+import "./sections/hero.js";
+import "./sections/skills.js";
+import "./sections/projects.js";
+import "./sections/contact.js";
+
+console.log("Glass portfolio loaded.");
---
+++ scripts/sections/contact.js
@@ -0,0 +1,2 @@
+// hero.js
+export default {};
